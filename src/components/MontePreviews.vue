31c964b990e41c90bd5c1f6e49d07e6b2b1b06f6
SimonettiT <contacto@simonettitomas.com>
2024-10-23 16:10:16 -0300

    Added images and interactivity

--- src/components/MontePreviews.vue
+++ src/components/MontePreviews.vue
@@ -1,20 +1,66 @@
 <script setup>
 import { ref } from 'vue';
-const isSelected = ref(true);
+
+import Frame1 from '@/assets/images/frames/frame-01.jpg';
+import Frame2 from '@/assets/images/frames/frame-02.jpg';
+import Frame3 from '@/assets/images/frames/frame-03.jpg';
+import Frame4 from '@/assets/images/frames/frame-04.jpg';
+import Frame5 from '@/assets/images/frames/frame-05.jpg';
+import Frame6 from '@/assets/images/frames/frame-06.jpg';
+import Frame1Thumb from '@/assets/images/frames/frame-01-thumb.jpg';
+import Frame2Thumb from '@/assets/images/frames/frame-02-thumb.jpg';
+import Frame3Thumb from '@/assets/images/frames/frame-03-thumb.jpg';
+import Frame4Thumb from '@/assets/images/frames/frame-04-thumb.jpg';
+import Frame5Thumb from '@/assets/images/frames/frame-05-thumb.jpg';
+import Frame6Thumb from '@/assets/images/frames/frame-06-thumb.jpg';
+
+const frames = ref([
+    {
+        id: 1,
+        src: Frame1,
+        thumbSrc: Frame1Thumb
+    },
+    {
+        id: 2,
+        src: Frame2,
+        thumbSrc: Frame2Thumb
+    },
+    {
+        id: 3,
+        src: Frame3,
+        thumbSrc: Frame3Thumb
+    },
+    {
+        id: 4,
+        src: Frame4,
+        thumbSrc: Frame4Thumb
+    },
+    {
+        id: 5,
+        src: Frame5,
+        thumbSrc: Frame5Thumb
+    },
+    {
+        id: 6,
+        src: Frame6,
+        thumbSrc: Frame6Thumb
+    }
+])
+
+const selectedFrame = ref(frames.value[0]);
+
+const selectFrame = (frame) => {
+    selectedFrame.value = frame;
+}
 </script>
 
 <template>
     <section class="serie-preview">
         <div class="serie-preview__container container">
             <h3>FOTOGRAMAS DE LA SERIE WEB</h3>
-            <img src="" alt="" class="preview__images-big">
+            <img :src="selectedFrame.src" alt="" class="preview__images-big" loading="lazy" />
             <div class="preview__images-thumb">
-                <img :class="{selected: isSelected}" src="" alt="">
-                <img src="" alt="">
-                <img src="" alt="">
-                <img src="" alt="">
-                <img src="" alt="">
-                <img src="" alt="">
+                <img @click="selectFrame(frame)" v-for="frame in frames" :class="{ selected: selectedFrame === frame }" :src="frame.thumbSrc" alt="" loading="lazy">
             </div>
         </div>
     </section>
@@ -41,10 +87,12 @@ img
 .preview__images-thumb
     @include mixins.flex(row, center, center, wrap)
     gap: 0.5rem
+    border: 0
     img
         cursor: pointer
     .selected
-        border: 2px solid colors.$accent-light-2
+        transition: border 0.3s ease-in-out
+        border: 4px solid colors.$accent-light-2
 @media (max-width: variables.$bkp-medium)
     .serie-preview
         padding-block: variables.$section-gap
