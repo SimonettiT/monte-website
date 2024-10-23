9e5970bddb6b791971f49fdaf891d90822a689e8
Tomás Simonetti <tomysimonetti3@gmail.com>
2024-10-23 01:32:53 -0300

    Added background image

--- src/components/BackstageCalltoAction.vue
+++ src/components/BackstageCalltoAction.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+import BackstageBackground1 from '@/assets/images/backstage-portrait-1.jpg';
+import BackstageBackground2 from '@/assets/images/backstage-portrait-2.jpg';
 </script>
 
 <template>
-    <section class="backstage-actioncall">
+    <section class="backstage-actioncall" :style="{ backgroundImage: 'url(' + BackstageBackground1 + ')' }">
         <div class="backstage-actioncall__container container">
             <h2 class="backstage-actioncall__title">¿CÓMO SE REALIZÓ MONTE?</h2>
             <p class="backstage-actioncall__description">CONOCÉ TODO EL TRABAJO HECHO PARA LLEGAR A TU PANTALLA</p>
@@ -22,6 +23,9 @@
 section.backstage-actioncall
     padding-block: calc(variables.$section-gap * 5)
     background-color: colors.$light
+    background-size: cover
+    object-fit: cover
+    color: colors.$light
     h2
         font-weight: fonts.$font-bold
         line-height: 1
