6fc845b22b653d8a943ef31f3c37a17c5a2014a1
Tomás Simonetti <tomysimonetti3@gmail.com>
2024-10-24 00:01:52 -0300

    Added backstage

---
+++ src/components/Credits.vue
@@ -0,0 +1,156 @@
+<script setup>
+import { ref } from 'vue';
+
+const mainCrew = ref([
+    {
+        name: 'Josefina Bett',
+        img: 'josefina-bett.jpg',
+        roles: ['Directora', 'Productora', 'Guionista']
+    },
+    {
+        name: 'Martina Degiovanni',
+        img: 'martina-degiovanni.jpg',
+        roles: ['Directora', 'Productora', 'Guionista'],
+    },
+    {
+        name: 'Malena Llabot',
+        img: 'malena-llabot.jpg',
+        roles: ['Directora', 'Productora', 'Guionista'],
+    },
+    {
+        name: 'Felipe Lorenzatti',
+        img: 'mariana-wainstein.jpg',
+        roles: ['Directora', 'Productora', 'Guionista'],
+    },
+    {
+        name: 'Matías Elizondo',
+        img: 'mariana-wainstein.jpg',
+        roles: ['Directora', 'Productora', 'Guionista'],
+    },
+    {
+        name: 'Justina Vazquez',
+        img: 'mariana-wainstein.jpg',
+        roles: ['Directora', 'Productora', 'Guionista'],
+    },
+    {
+        name: 'Candela Nogueira',
+        img: 'mariana-wainstein.jpg',
+        roles: ['Directora', 'Productora', 'Guionista'],
+    },
+    {
+        name: 'Tomás Simonetti',
+        img: 'mariana-wainstein.jpg',
+        roles: ['Directora', 'Productora', 'Guionista'],
+    },
+]);
+const crew = ref([
+    {
+        name: 'Mariana Wainstein',
+        role: 'Alfonsina'
+    },
+    {
+        name: 'Mariana Wainstein',
+        role: 'Alfonsina'
+    },
+    {
+        name: 'Mariana Wainstein',
+        role: 'Alfonsina'
+    },
+    {
+        name: 'Mariana Wainstein',
+        role: 'Alfonsina'
+    },
+    {
+        name: 'Mariana Wainstein',
+        role: 'Alfonsina'
+    },
+    {
+        name: 'Mariana Wainstein',
+        role: 'Alfonsina'
+    },
+    {
+        name: 'Mariana Wainstein',
+        role: 'Alfonsina'
+    },
+    {
+        name: 'Mariana Wainstein',
+        role: 'Alfonsina'
+    },
+])
+</script>
+
+<template>
+    <section class="credits">
+        <div class="credits__container container">
+            <h2>CONOCE A LOS CREADORES</h2>
+
+            <div class="credits__main-crew">
+                <div v-for="person in mainCrew" class="credits__person">
+                    <img :src="`../assets/images/crew/${person.img}`" alt="">
+                    <h4>{{ person.name }}</h4>
+                    <p v-for="role in person.roles">{{ role }}</p>
+                </div>
+            </div>
+            <div class="credits__crew">
+                <p v-for="person in crew" class="credits__person">
+                    <span class="person-name">{{ person.name }}</span>
+                     - 
+                    <span class="person-role">{{ person.role }}</span>
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="sass" scoped>
+@use "@/style/fonts"
+@use "@/style/colors"
+@use "@/style/mixins"
+@use "@/style/variables"
+
+section.credits
+    background-color: colors.$white
+    color: colors.$dark
+    padding-block: 5rem 7rem
+.credits__main-crew
+    @include mixins.grid(4, 2, stretch, center)
+    gap: 2rem
+    margin-bottom: calc(variables.$section-gap * 2)
+    @media (max-width: variables.$bkp-small-medium)
+        @include mixins.grid(3, 1, stretch, center)
+    @media (max-width: variables.$bkp-small)
+        @include mixins.grid(2, 1, stretch, center)
+    img
+        width: 100%
+        margin-bottom: 0.5rem
+        background-color: colors.$light-shadow-1
+        width: 100%
+        height: auto
+        aspect-ratio: 1
+    h4
+        font-size: fonts.$font-md
+        height: 4rem
+        padding-block: auto
+        transition: color 0.5s
+    .credits__person:hover h4
+        color: colors.$accent
+    p
+        font-size: fonts.$font-base
+
+
+.credits__crew
+    @include mixins.flex(row, space-evenly, center, wrap)
+    gap: 0.5rem
+    .credits__person:hover .person-name
+        color: colors.$accent
+
+    .person-name
+        font-weight: bold
+        transition: color 0.5s
+    .person-role
+        text-transform: uppercase
+        font-weight: normal
+
+        
+
+</style>
--- src/views/Backstage.vue
+++ src/views/Backstage.vue
@@ -1,12 +1,95 @@
 <script setup>
+import Credits from '@/components/Credits.vue';
+import MontePreviews from '@/components/MontePreviews.vue';
+
 
 </script>
 <template>
-    <div>
-        <h1>BACKSTAGE</h1>
+    <div class="backstage">
+        <header class="backstage-cover">
+            <h1>¿CÓMO SE REALIZÓ MONTE?</h1>
+            <p>CONOCE A LOS CREADORES DE LA SERIE Y TODO EL TRABAJO HASTA TU PANTALLA</p>
+            <button class="btn">VER EL VIDEO DETRÁS DE ESCENA</button>
+
+        </header>
+        <main>
+
+            <MontePreviews title="LO QUE PASÓ DETRÁS DE CÁMARA"/>
+            <Credits />
+
+            <section class="production-numbers">
+                <div class="production-numbers__container container">
+                    <h2>NÚMEROS DE MONTE</h2>
+                    <div class="production__item">
+                        <h3>$1.456.138 ARS | $1462 USD</h3>
+                        <h5 class="subtitle-accent">Presupuesto total de la serie</h5>
+                    </div>
+
+
+                    <div class="production__item">
+                        <h3>531 PERSONAS</h3>
+                        <h5 class="subtitle-accent">implicadas en la serie</h5>
+                        <p>8 creadores</p>
+                        <p>20 asistentes</p>
+                        <p>6 actores</p>
+                        <p>45 extras actorales</p>
+                        <p>342 inversores en la serie</p>
+                    </div>
+
+
+                    <div class="production__item">
+                        <h3>3 DÍAS DE <span class="subtitle-accent">RODAJE</span></h3>
+                        <p>26 días de Pre-Producción</p>
+                        <p>17 días de Post-Producción</p>
+                    </div>
+
+                    <div class="production__item">
+                        <h3>3 <span class="subtitle-accent">CAPÍTULOS</span></h3>
+                        <p>2 Locaciones</p>
+                        <p>158 tomas</p>
+                        <p>239GB de información</p>
+                    </div>
+                </div>
+            </section>
+
+        </main>
     </div>
 </template>
 
 <style lang="sass">
-    
+@use '@/style/colors'
+@use '@/style/fonts'
+@use '@/style/mixins'
+@use '@/style/variables'
+
+.backstage
+    color: colors.$light
+    header.backstage-cover
+        @include mixins.flex(column, center, center, nowrap)
+        width: 100%
+        min-height: 500px
+        aspect-ratio: 16/9
+        h1
+            font-size: fonts.$font-xxxl
+            line-height: 1
+        p
+            line-height: 1.5
+            margin-bottom: 2rem
+
+.production-numbers
+    background-image: linear-gradient(180deg, colors.$dark-shadow 0%, colors.$dark 100%)
+    padding-block: 5rem
+    .production__item
+        margin-top: 40vh
+        &:nth-child(odd)
+            text-align: right
+        &:nth-child(even)
+            text-align: left
+        h3
+            line-height: 1
+        .subtitle-accent
+            color: colors.$accent
+            font-weight: fonts.$font-bold
+        h5, p
+            font-weight: fonts.$font-semibold
 </style>
