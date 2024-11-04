9cab6494792aa2d773d26b2063a5f3415cb90841
Tomás Simonetti <tomysimonetti3@gmail.com>
2024-11-03 22:24:30 -0300

    Added Sponsors link

--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Chapters.vue')
     },
+    {
+      path: '/sponsors',
+      name: 'Sponsors',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Financing.vue')
+    },
   ],
   scrollBehavior(to, from, savedPosition) {
     return {
